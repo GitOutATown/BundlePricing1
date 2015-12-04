c0836a1156b1051d489ec551bb1a286b5b7048e5
GitOutATown <aqumegeemail@gmail.com>
2015-12-03 19:03:03 -0800

    Adding BundleService

--- src/main/scala/bundle_pricing/lab2/BundlePricing_lab2.scala
+++ src/main/scala/bundle_pricing/lab2/BundlePricing_lab2.scala
@@ -1,9 +1,36 @@
 package bundle_pricing.lab2
 
+/**
+ * TODO: Validators: Option/Either/Try
+ * asynchronous
+ * tests
+ */
+
 case class Item(
     id: String,
     price: Double
 )
+
+case class Cart(
+    items: List[Item]
+)
+
+object CartService {
+    def addItem(item: Item, cart: Cart): Cart = {
+        cart.copy(items = item :: cart.items)
+    }
+    
+    def addItems(item: Item, qty: Int, cart: Cart): Cart = {
+        val items = List.fill(qty)(item)
+        cart.copy(items = items ++ cart.items)
+    }
+    
+    // Calculates minimum price per bundle discounts
+    // Prints line items and discounts applied
+    def checkout(cart: Cart): Double = {
+        0 // STUB
+    }
+}
 
 trait Bundle
 trait Discount
@@ -29,23 +56,23 @@ case class AppliedHow(
     grouping: Bundle
 )
 
-case class Cart(
-    items: List[Item]
-)
-
-object CartService {
-    def addItem(item: Item, cart: Cart): Cart = {
-        cart.copy(items = item :: cart.items)
+// Create Bundles
+object BundleService {
+    def qtyForPrice(item: Item, qty: Int, price: Double): Bundle = {
+        val part = BundlePart(item, qty)
+        val qualifier = List(part)
+        val discount = AppliedHow(FlatPrice(price), part)
+        BundleWhole(qualifier, discount)
     }
     
-    def addItems(item: Item, qty: Int, cart: Cart): Cart = {
-        val items = List.fill(qty)(item)
-        cart.copy(items = items ++ cart.items)
-    }
-    
-    // Calculates minimum price per bundle discounts
-    // Prints line items and discounts applied
-    def checkout(cart: Cart): Double = {
-        0 // STUB
+    def multiPart(
+        allParts: List[BundlePart],
+        discount: Discount,
+        appliedTo: BundlePart
+    ): Bundle = {
+        // TODO: Verify/Require that appliedTo exists in parts
+        val appHow = AppliedHow(discount, appliedTo)
+        BundleWhole(allParts, appHow)
     }
 }
+
