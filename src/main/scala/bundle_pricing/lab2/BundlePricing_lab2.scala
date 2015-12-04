bed9ce27a6d6ae6a5a04ba79e76352d6b4a13c76
GitOutATown <aqumegeemail@gmail.com>
2015-12-03 19:40:33 -0800

    Consolidated quantity into Item

--- src/main/scala/bundle_pricing/lab2/BundlePricing_lab2.scala
+++ src/main/scala/bundle_pricing/lab2/BundlePricing_lab2.scala
@@ -1,15 +1,19 @@
 package bundle_pricing.lab2
 
 /**
- * TODO: Validators: Option/Either/Try
+ * TODO:
+ * Calculate minPrice 
+ * Validators: Option/Either/Try
+ * print all line items and discounts
  * asynchronous
  * tests
  */
 
 case class Item(
     id: String,
-    price: Double
-)
+    price: Double,
+    qty: Int = 1
+) extends Bundleable
 
 case class Cart(
     items: List[Item]
@@ -28,51 +32,47 @@ object CartService {
     // Calculates minimum price per bundle discounts
     // Prints line items and discounts applied
     def checkout(cart: Cart): Double = {
-        0 // STUB
+        0 // TODO: STUB
     }
 }
 
-trait Bundle
+trait Bundleable
 trait Discount
 
 case class PercentOff(pct: Double) extends Discount
 case class FlatPrice(flat: Double) extends Discount
 
-case class BundlePart(
-    item: Item,
-    qty: Int
-) extends Bundle
-
 // Aggregation of required items, with applied discount
-case class BundleWhole(
-    qualifiers: List[BundlePart],
+case class Bundle(
+    qualifier: List[Item],
     discount: AppliedHow,
     price: Double = 0
-) extends Bundle
+) extends Bundleable
 
 // What kind of discount applied to what
 case class AppliedHow(
-    disc: Discount,
-    grouping: Bundle
+    discount: Discount,
+    grouping: Bundleable
 )
 
 // Create Bundles
 object BundleService {
-    def qtyForPrice(item: Item, qty: Int, price: Double): Bundle = {
-        val part = BundlePart(item, qty)
-        val qualifier = List(part)
-        val discount = AppliedHow(FlatPrice(price), part)
-        BundleWhole(qualifier, discount)
+    
+    // Item has quantity
+    def qtyForPrice(qualifier: Item, price: Double): Bundleable = {
+        val discount = AppliedHow(FlatPrice(price), qualifier)
+        Bundle(List(qualifier), discount)
     }
     
     def multiPart(
-        allParts: List[BundlePart],
+        otherParts: List[Item],
         discount: Discount,
-        appliedTo: BundlePart
-    ): Bundle = {
+        appliedTo: Item
+    ): Bundleable = {
         // TODO: Verify/Require that appliedTo exists in parts
         val appHow = AppliedHow(discount, appliedTo)
-        BundleWhole(allParts, appHow)
+        val allParts = appliedTo :: otherParts
+        Bundle(allParts, appHow)
     }
 }
 
