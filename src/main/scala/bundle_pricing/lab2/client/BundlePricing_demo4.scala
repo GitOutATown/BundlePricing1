02c98150ccb7a629a63d1bed5e2ad45505bd51e8
GitOutATown <aqumegeemail@gmail.com>
2015-12-08 15:54:52 -0800

    Adding flat price demo that fails on missing additional qualifier

--- src/main/scala/bundle_pricing/lab2/client/BundlePricing_demo4.scala
+++ src/main/scala/bundle_pricing/lab2/client/BundlePricing_demo4.scala
@@ -6,25 +6,27 @@ import bundle_pricing.lab2.app.BundleService._
 import scala.util.{Success, Failure}
 import scala.concurrent.ExecutionContext.Implicits.global
 
-object BundlePricing_demo3 extends App {
+object BundlePricing_demo4 extends App {
     
     /**
-     * In this demo the cart meets the flat price bundle requirements
-     * and set one flat price for all of the applied items. No other
-     * additional qualifiers are included.
+     * Demonstrates dependency on additional qualifiers, i.e.
+     * middle brackets (which can be empty). In this demo the
+     * bundle wants an additional qualifier with quantity of 5,
+     * but the cart only has 4.
      */
 
     val i1 = Item("One", 1.0)
     val i2 = Item("Two", 1.0)
     val i3 = Item("Three", 1.0)
+    val addQual = Item("Missing", 1.0)
     
-    val shoppingList = List((i1, 3), (i2, 1), (i3, 2))
+    val shoppingList = List((i1, 3), (i2, 1), (i3, 2), (addQual, 4))
     
     val cart = getCart
     
     val cart2 = addToCart(shoppingList, cart)
     
-    val flatPrice = bundlePrice(3.0, (i1, 3), (i2, 1), (i3, 2))()("Test flat price.")
+    val flatPrice = bundlePrice(3.0, (i1, 3), (i2, 1), (i3, 2))((addQual, 5))("Test flat price.")
 
     checkout(cart2, List(flatPrice)).onComplete {
         case Success(cart) => printReceipt(cart)
