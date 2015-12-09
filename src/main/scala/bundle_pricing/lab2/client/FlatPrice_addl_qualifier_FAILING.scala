7e972c9d00327e40050bdc0e5d4d202a0b2550c9
GitOutATown <aqumegeemail@gmail.com>
2015-12-08 16:12:50 -0800

    Failing test for flat price with additional qualifier.

---
+++ src/main/scala/bundle_pricing/lab2/client/FlatPrice_addl_qualifier_FAILING.scala
@@ -0,0 +1,40 @@
+package bundle_pricing.lab2.client
+
+import bundle_pricing.lab2.app._
+import bundle_pricing.lab2.app.CartService._
+import bundle_pricing.lab2.app.BundleService._
+import scala.util.{Success, Failure}
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object BundlePricing_demo5 extends App {
+    
+    /**
+     * THIS TEST IS FAILING.
+     * Demonstrates dependency on additional qualifiers, i.e.
+     * middle brackets (which can be empty). In this demo the
+     * bundle wants an additional qualifier with quantity of 5
+     * and finds what it needs.
+     */
+
+    val i1 = Item("One", 1.0)
+    val i2 = Item("Two", 1.0)
+    val i3 = Item("Three", 1.0)
+    val addQual = Item("Not Missing", 1.0)
+    
+    val shoppingList = List((i1, 3), (i2, 1), (i3, 2), (addQual, 10))
+    
+    val cart = getCart
+    
+    val cart2 = addToCart(shoppingList, cart)
+    
+    val flatPrice = bundlePrice(3.0, (i1, 3), (i2, 1), (i3, 2))((addQual, 5))("Test flat price.")
+
+    checkout(cart2, List(flatPrice)).onComplete {
+        case Success(cart) => printReceipt(cart)
+        case Failure(e) => println(e) 
+    }
+    
+    Thread.sleep(3000)
+}
+
+
