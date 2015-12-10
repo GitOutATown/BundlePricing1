96f7a7c1cd67550b83697ce3f609df4c84c7922d
GitOutATown <aqumegeemail@gmail.com>
2015-12-09 23:13:09 -0800

    Minor failure message edit.

--- src/main/scala/bundle_pricing/lab2/client/AsynchronousCalls_demo10.scala
+++ src/main/scala/bundle_pricing/lab2/client/AsynchronousCalls_demo10.scala
@@ -20,9 +20,7 @@ object AsynchronousCalls_demo10 extends App {
 
     val shoppingList = List((i1, 4), (i2, 3), (i3, 3), (addQual, 1))
     
-    val cart = getCart
-    
-    val cart2 = addToCart(shoppingList, cart)
+    val cart = addToCart(shoppingList, getCart)
     
     val flat2 = bundlePrice(1.75, (i2, 2))()("2 i2 for $1.75")
     val flat3 = bundlePrice(4.00, (i1, 2),(i3, 2))()("2 i1 and 2 i3 for $4.00")
@@ -44,11 +42,11 @@ object AsynchronousCalls_demo10 extends App {
     
     // Fire off all bundles in separate checkouts
     bundles.foreach{                   
-        checkout(cart2, _).onComplete {
+        checkout(cart, _).onComplete {
             case Success(cart) =>
                 receipt(cart).onComplete{
                     case Success(r) => println(r)
-                    case Failure(e) => println(s"Failed to print recipt: $e")
+                    case Failure(e) => println(s"Failed to tender recipt: $e")
                 }
             case Failure(e) => println(s"Checkout failed: $e") 
         }
